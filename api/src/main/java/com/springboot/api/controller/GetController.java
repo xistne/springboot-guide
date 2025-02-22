5b00b257e0843745e0ee073c60dc7958e90b9121
xistne <du20063@gmail.com>
2025-02-22 15:55:27 +0900

    api

---
+++ api/src/main/java/com/springboot/api/controller/GetController.java
@@ -0,0 +1,62 @@
+package com.springboot.api.controller;
+
+import com.springboot.api.dto.MemberDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+    // http://localhost:8080/api/v1/get-api/hello
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        return "Hello World";
+    }
+
+    // http://localhost:8080/api/v1/get-api/name
+    @GetMapping(value = "/name")
+    public String getName() {
+        return "Flature";
+    }
+
+    // http://localhost:8080/api/v1/get-api/variable1/{String 값}
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    // http://localhost:8080/api/v1/get-api/variable2/{String 값}
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var) {
+        return var;
+    }
+
+    // http://localhost:8080/api/v1/get-api/request1?name=value1&email=value2&organization=value3
+    @Operation(summary = "GET 메서드 예제", description = "@RequestParam을 활용한 GET Method")
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @Parameter(description = "이름", required = true) @RequestParam String name,
+            @Parameter(description = "이메일", required = true) @RequestParam String email,
+            @Parameter(description = "회사", required = true) @RequestParam String organization
+    ) {
+        return name + " " + email + " " + organization;
+    }
+
+    // http://localhost:8080/api/v1/get-api/request2?key1=value1&key2=value2&key3=value3
+    @GetMapping(value="/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+        return sb.toString();
+    }
+    // http://localhost:8080/api/v1/get-api/request3?name=value1&email=value2&organization=value3
+    @GetMapping(value = "/request3")
+    public String getRequestParam3(MemberDto memberDto) {
+        return memberDto.toString();
+    }
+}
