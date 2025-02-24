800d4c7f4a9e93ceea5ca1cb27b26f17eebd4f1f
xistne <du20063@gmail.com>
2025-02-25 00:53:28 +0900

    Chore : lombok

--- jpa/src/main/java/com/springboot/jpa/data/dto/ChangeProductNameDto.java
+++ jpa/src/main/java/com/springboot/jpa/data/dto/ChangeProductNameDto.java
@@ -1,27 +1,13 @@
 package com.springboot.jpa.data.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ChangeProductNameDto {
     private Long number;
     private String name;
-
-    public ChangeProductNameDto(Long number, String name) {
-        this.number = number;
-        this.name = name;
-    }
-
-    public Long getNumber() {
-        return number;
-    }
-
-    public void setNumber(Long number) {
-        this.number = number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
