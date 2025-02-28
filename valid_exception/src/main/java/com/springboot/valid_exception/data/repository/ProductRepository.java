fdfbf641dc8009b43cd79a9b0ae889b7fd210be4
xistne <du20063@gmail.com>
2025-02-28 13:53:01 +0900

    Feat : 프로젝트 초기 세팅

---
+++ valid_exception/src/main/java/com/springboot/valid_exception/data/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.valid_exception.data.repository;
+
+import com.springboot.valid_exception.data.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
