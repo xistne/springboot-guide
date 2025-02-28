630a19d896264a41f08b6ac677acd78891160874
xistne <du20063@gmail.com>
2025-02-28 13:53:31 +0900

    Feat : @Valid

---
+++ valid_exception/src/main/java/com/springboot/valid_exception/controller/ValidationController.java
@@ -0,0 +1,30 @@
+package com.springboot.valid_exception.controller;
+
+import com.springboot.valid_exception.data.dto.ValidRequestDto;
+import com.springboot.valid_exception.data.dto.ValidatedRequestDto;
+import com.springboot.valid_exception.data.group.ValidationGroup1;
+import com.springboot.valid_exception.data.group.ValidationGroup2;
+import jakarta.validation.Valid;
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/validation")
+public class ValidationController {
+    private final Logger LOGGER = LoggerFactory.getLogger(ValidationController.class);
+
+    @PostMapping("/valid")
+    public ResponseEntity<String> checkValidationByValid(
+            @Valid @RequestBody ValidRequestDto validRequestDto) {
+        LOGGER.info(validRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validRequestDto.toString());
+    }
+}
