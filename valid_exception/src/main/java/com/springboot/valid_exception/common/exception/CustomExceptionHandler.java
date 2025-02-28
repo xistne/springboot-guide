da8a4bfb73a50601ae7f2270b0be13b714095313
xistne <du20063@gmail.com>
2025-02-28 15:21:52 +0900

    Feat: 예외처리

---
+++ valid_exception/src/main/java/com/springboot/valid_exception/common/exception/CustomExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.springboot.valid_exception.common.exception;
+
+import com.springboot.valid_exception.controller.ExceptionController;
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Hidden
+@RestControllerAdvice
+public class CustomExceptionHandler {
+    private final Logger LOGGER = LoggerFactory.getLogger(CustomExceptionHandler.class);
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handlerException(RuntimeException e, HttpServletRequest request) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        LOGGER.error("Advice 내 HandlerException 호출, {}, {}", request.getRequestURI(), e.getMessage());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("message", e.getMessage());
+
+        return new ResponseEntity<>(map, responseHeaders, httpStatus);
+    }
+}
