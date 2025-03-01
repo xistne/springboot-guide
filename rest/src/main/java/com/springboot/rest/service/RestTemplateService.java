33d62d75e6fa65d7635c8cb67807efa1edfa2b8a
xistne <du20063@gmail.com>
2025-03-01 16:50:18 +0900

    Feat : RestTemplate 커스텀 설정

--- rest/src/main/java/com/springboot/rest/service/RestTemplateService.java
+++ rest/src/main/java/com/springboot/rest/service/RestTemplateService.java
@@ -1,8 +1,12 @@
 package com.springboot.rest.service;
 
 import com.springboot.rest.dto.MemberDto;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -96,5 +100,25 @@ public class RestTemplateService {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<MemberDto> responseEntity = restTemplate.exchange(requestEntity, MemberDto.class);
         return responseEntity;
+    }
+
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        PoolingHttpClientConnectionManager poolingConnManager = new PoolingHttpClientConnectionManager();
+        poolingConnManager.setMaxTotal(500);
+        poolingConnManager.setDefaultMaxPerRoute(500);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(poolingConnManager)
+                .build();
+
+        factory.setHttpClient(httpClient);
+        factory.setConnectTimeout(2000);
+        factory.setReadTimeout(5000);
+
+        RestTemplate restTemplate = new RestTemplate(factory);
+
+        return restTemplate;
     }
 }
