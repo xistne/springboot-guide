58a971ac6122c6a97dfe75617578fb2f5705d534
xistne <du20063@gmail.com>
2025-03-02 21:33:46 +0900

    Feat : UserDetails, UserDetailsService 구현

---
+++ security/src/main/java/com/springboot/security/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,24 @@
+package com.springboot.security.service.impl;
+
+import com.springboot.security.data.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    private final Logger LOGGER = LoggerFactory.getLogger(UserDetailServiceImpl.class);
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        LOGGER.info("[loadUserByUsername] loadUserByUsername 수행. username : {}", username);
+        return userRepository.getByUid(username);
+    }
+
+}
